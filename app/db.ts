87b1394a29b96c20cbe457ea897215832613bee7
Patrique Ouimet <patrique.ouimet@gmail.com>
2021-12-11 10:45:22 -0500

    Clean up

--- app/db.ts
+++ app/db.ts
@@ -1,4 +1,3 @@
-// import { RouteHandler } from "../routes.ts"
 import { Client } from "https://deno.land/x/postgres/mod.ts";
 
 export const client = new Client({
@@ -11,20 +10,5 @@ export const client = new Client({
       enforce: false
   }
 });
+
 await client.connect();
-
-// export const client;
-
-// const arrayResult = await client.queryArray("SELECT ID, NAME FROM PEOPLE");
-// console.log(arrayResult.rows); // [[1, 'Carlos'], [2, 'John'], ...]
-
-// const objectResult = await client.queryObject("SELECT ID, NAME FROM PEOPLE");
-// console.log(objectResult.rows); // [{id: 1, name: 'Carlos'}, {id: 2, name: 'John'}, ...]
-
-// await client.end();
-
-// export default class DbHandler implements RouteHandler {
-//     handle(request: Request): Response {
-//         return new Response("DB page!", { status: 200 });
-//     }
-// }
