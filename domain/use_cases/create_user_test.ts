d584319e155cfeb9e9de1f822cf4b405310cfa6b
Patrique Ouimet <patrique.ouimet@gmail.com>
2021-12-23 21:46:53 -0500

    Added first unit test

---
+++ domain/use_cases/create_user_test.ts
@@ -0,0 +1,51 @@
+import { delay } from "https://deno.land/std@0.119.0/async/delay.ts";
+import { format } from "https://deno.land/std@0.119.0/datetime/mod.ts";
+import { assert, assertEquals } from "https://deno.land/std@0.119.0/testing/asserts.ts";
+import { NewUser, User, UserRepositoryInterface } from "../user.ts";
+import CreateUser from "./create_user.ts";
+
+class MockUserRepository implements UserRepositoryInterface {
+    public data: User[] = [];
+
+    doesUserExists = async (email: string): Promise<boolean> => {
+        await delay(1);
+        return Boolean(this.data.find(user => user.email = email)).valueOf();
+    }
+
+    createUser = async (newUser: NewUser): Promise<boolean> => {
+        const now = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+        this.data.push(
+            new User(
+                this.data.length + 1,
+                newUser.name,
+                newUser.email,
+                now,
+                now
+            )
+        );
+        await delay(1);
+        return true;
+    }
+}
+
+Deno.test("Create User", async () => {
+    // Arrange
+    const mockUserRepository = new MockUserRepository();
+    const createUserCase = new CreateUser(mockUserRepository);
+    const newUser = new NewUser(
+        'John Doe',
+        'johndoe@gmail.com',
+        'foobar'
+    );
+
+    // Pre-assert
+    // user should not exists at this point
+    assert(!await mockUserRepository.doesUserExists(newUser.email));
+
+    // Act
+    await createUserCase.handle(newUser);
+
+    // Assert
+    // verify user has been created and exist
+    assert(await mockUserRepository.doesUserExists(newUser.email));
+});
