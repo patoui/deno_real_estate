ccf089a01d110571dbfc69b217e6177e260e88ed
Patrique Ouimet <patrique.ouimet@gmail.com>
2022-01-17 08:36:32 -0500

    Added salt to hashing

--- application/services/hasher.ts
+++ application/services/hasher.ts
@@ -2,10 +2,12 @@ import { crypto } from '../../deps.ts';
 
 export default class Hasher {
   hash = async (data: string): Promise<string> => {
+    // TODO move to env config
+    const salt = '8CgFZc(4Rl/V+Ce9';
     return Hasher.toHexString(
         await crypto.subtle.digest(
             "BLAKE3",
-            new TextEncoder().encode(data)
+            new TextEncoder().encode(salt + data)
         )
     );
   }
